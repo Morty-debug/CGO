46f1eb7219e418b388ef88edd7ac0ae81a3f4394
Morty MCFLY <77272753+Morty-debug@users.noreply.github.com>
2022-02-02 08:02:23 -0600

    Update main.c

--- main.c
+++ main.c
@@ -2,13 +2,13 @@
 
 #ifdef gcc
 #include "Print.h"
-int cmain(int argc, char *argv[]);
+int cmain(int argc, char *argv[]); //metodo cmain que esta fuera de la condicion
 int main(int argc, char *argv[]){
 	printf("Desde C ");
 	return cmain(argc,argv);
 }
 #else
-extern void Print(char* argumento);
+extern void Print(char* argumento); //metodo Print que esta en main.go
 #endif
 
 
