5d0993903f6a5e98ba1c1f8d899f46043e6784e4
Morty MCFLY <77272753+Morty-debug@users.noreply.github.com>
2022-02-06 20:00:06 -0600

    Update main.go

--- main.go
+++ main.go
@@ -11,5 +11,5 @@ func Print(argumento *C.char){
 func main(){
 	fmt.Print("Desde Go ")
 	cmain() //metodo que esta en gcc.go y en nogcc.go 
-		//se invoca segun sea el argumento de compilador, si usara C no lo usara
+		//se invoca segun sea el argumento del compilador, si usara C no lo usara
 }
